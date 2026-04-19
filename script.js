5087a0b976e5a8d58e7989d8ead8a70d65241e98
ishashivluv-png <ishashivluv@gmail.com>
2026-04-19 14:57:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,4 +62,5 @@ function submitWaste() {
 }
 window.onload = function() {
     document.getElementById("login").style.display = "block";
+    document.getElementById("qr").style.display = "none";
 };
